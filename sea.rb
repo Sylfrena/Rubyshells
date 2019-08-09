6430892638212a0882b2e213496d5e95607e85ab
Sumera Priyadarsini <sylphrenadin@gmail.com>
2019-08-10 00:33:33 +0530

    sigh again

--- sea.rb
+++ sea.rb
@@ -1,20 +1,12 @@
 require 'socket'
 
-
-
-
 SERVER_IP = 'seashells.io'
 SERVER_PORT = 1337
 
-
-
-
 server = TCPSocket.new(SERVER_IP, SERVER_PORT)
 
-while line=STDIN.gets
-
-	server.write line
-	puts server.read(42)
+while true
+	server.write STDIN.gets
+	puts server.gets
 end
-
 server.close
