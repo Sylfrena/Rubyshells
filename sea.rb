4ceec17e7290e35e6359345b398730002ef0e7f3
Sumera Priyadarsini <sylphrenadin@gmail.com>
2019-08-09 23:33:11 +0530

    sigh

---
+++ sea.rb
@@ -0,0 +1,20 @@
+require 'socket'
+
+
+
+
+SERVER_IP = 'seashells.io'
+SERVER_PORT = 1337
+
+
+
+
+server = TCPSocket.new(SERVER_IP, SERVER_PORT)
+
+while line=STDIN.gets
+
+	server.write line
+	puts server.read(35)
+end
+
+server.close
